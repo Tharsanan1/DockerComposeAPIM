18e3d9b5a0898709719d3826b5d0f5857201c15c
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-11-25 19:13:32 +0530

    Add 4.0.0 changes

--- main.sh
+++ main.sh
@@ -3,16 +3,16 @@ echo "Running main.sh"
 if [ $# -eq 0 ]
     then
         echo "No arguments supplied, hence starting APIM as single server"
-        sh wso2am-3.2.0/bin/wso2server.sh
+        sh wso2am-4.0.0/bin/wso2server.sh
     else
         echo "Argument received: $1"
         echo "Rrying to setup profile"
-        sh wso2am-3.2.0/bin/profileSetup.sh "$1"
+        sh wso2am-4.0.0/bin/profileSetup.sh "$1"
         # Remove mysql entry
-        sed -i -- 's/\[database.apim_db\]/\[test.test1\]/g' wso2am-3.2.0/repository/conf/deployment.toml
-        sed -i -- 's/\[database.shared_db\]/\[test.test2\]/g' wso2am-3.2.0/repository/conf/deployment.toml
-        echo "" >> wso2am-3.2.0/repository/conf/deployment.toml
-        cat h2-database-config.toml >> wso2am-3.2.0/repository/conf/deployment.toml
-        cat wso2am-3.2.0/repository/conf/deployment.toml
-        sh wso2am-3.2.0/bin/wso2server.sh
+        sed -i -- 's/\[database.apim_db\]/\[test.test1\]/g' wso2am-4.0.0/repository/conf/deployment.toml
+        sed -i -- 's/\[database.shared_db\]/\[test.test2\]/g' wso2am-4.0.0/repository/conf/deployment.toml
+        echo "" >> wso2am-4.0.0/repository/conf/deployment.toml
+        cat h2-database-config.toml >> wso2am-4.0.0/repository/conf/deployment.toml
+        cat wso2am-4.0.0/repository/conf/deployment.toml
+        sh wso2am-4.0.0/bin/wso2server.sh
 fi
